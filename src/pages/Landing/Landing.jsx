5504dc2e4853fc5440629d51ea1ccea08938dc4f
choijyoung <choi.jae.y@gmail.com>
2022-03-24 13:23:55 -0400

    fix landing link

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -7,10 +7,7 @@ const Landing = () => {
       <h1>All-The-Things</h1>
       <Link to="/the-manliest-things">Ben's Things</Link><br/>
       <Link to="/the-functional-things">Shahzad's Things</Link><br/>
-      <Link to="/the-well-styled-things">David's Things</Link><br/>
-    
-      <Link to="/the-silly-things">Mandy's Things</Link><br/>
-
+      <Link to="/the-well-styled-things">Mandy's Things</Link><br/>
       <Link to="/the-silly-things">Jaeyoung's Things</Link><br/>
     </>
   )
